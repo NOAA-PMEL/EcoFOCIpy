0dfc94a8c125612baa2aabca94f44eb6d9b271c7
Shaun Bell <shaun.bell@noaa.gov>
2021-03-25 12:16:02 -0700

    packaging and io testing

---
+++ HISTORY.rst
@@ -0,0 +1,12 @@
+.. :changelog:
+
+History
+-------
+
+0.0.1
+-----
+
+*Release date: N/A*
+
+* Initial import.
+* Begin transferring historic tools and setting up package
--- src/EcoFOCIpy/io/wpak_parser.py
+++ src/EcoFOCIpy/io/wpak_parser.py
@@ -1,4 +1,3 @@
-# EcoFOCI
 """Contains a collection of WPAK equipment parsing.
 
 These include:
@@ -6,4 +5,48 @@ These include:
 * ARGO Transmitted
 * Internally Recorded
 
+    usage: where src=instrument class
+    fobj = src.get_data(filename, MooringID)
+    Dataset = src.parse(fobj, **kwargs)
 """
+from io import BytesIO
+
+import pandas as pd
+
+
+class wpak(object):
+    r""" MetOcean WeatherPak
+    
+    remove software lines up to header-row for downloaded data
+
+    see staticdata/wpak_test.txt for example
+    """
+
+    @staticmethod
+    def get_data(filename=None, **kwargs):
+        r"""
+        Basic Method to open files.  Specific actions can be passes as kwargs for instruments
+        """
+
+        fobj = open(filename)
+        data = fobj.read()
+
+        buf = data
+        return BytesIO(buf.strip())
+
+    @staticmethod
+    def parse(fobj, argos_file=False):
+        r"""
+        Basic Method to open and read wpak csv files 
+
+        Alternatively we can pass ARGOS retrieved data in 
+
+        returns pandas dataframe
+        """
+
+        rawdata_df = pd.read_csv(fobj, header=0, delimiter="s+")
+        rawdata_df["date_time"] = pd.to_datetime(
+            rawdata_df["DATE"] + " " + rawdata_df["TIME"], format="%y/%m/%d %H:%M:%S"
+        )
+
+        return rawdata_df
