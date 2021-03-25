0f42e5efe42de3f91602facc7b77eb87450255f8
Shaun Bell <shaun.bell@noaa.gov>
2021-03-25 14:28:40 -0700

    wpak intial analysis

--- examples/wpak_process.py
+++ examples/wpak_process.py
@@ -7,9 +7,36 @@ Demonstrate how to use the routines to process downloaded wpak data.
 Not valid for argos data (NRT - only)
 """
 
-from EcoFOCIpy import io.wpak_parser
+import src.EcoFOCIpy.io.wpak_parser as wpak_parser
 
 testfile = '..staticdata/wpak_test.txt'
-wpak = wpak_parser()
 
-wpak.get_data(filename=testfile).parse()
+#init and load data
+wpak = wpak_parser.wpak()
+wpak_data = wpak.parse(filename=testfile,datetime_index=True) 
+
+#time resample (or decimate)
+
+"""
+absolute simplest time gridding
+ resample hourly will effectively truncate our times (only one data point in each hour)
+ interpolate will fill some gaps
+### like a time shift without adjusting data"""
+wpak_grid_simple = wpak_data.resample('1H').mean().interpolate(limit=3)
+
+"""
+slightly more complex method - valid when data is far enough from the hour
+ resample every 10min - but then linearly fill the supersampling
+ then decimate to the hour
+### very simple linear interpolation to hour"""
+wpak_grid_decimate = wpak_data.resample('10T').mean().interpolate(limit=6)
+wpak_grid_decimate = wpak_grid_decimate[wpak_grid_decimate.index.minute == 0]
+
+# Ingest instrumenttype parameter config file for meta information
+# Ingest mooring yaml status file for deployment meta information
+
+
+
+# tests 
+def sample_data_size_test(data=wpak_data):
+    assert wpak_data.shape == (83, 13), 'test file not read in as expected - shape not right'
--- src/EcoFOCIpy/io/wpak_parser.py
+++ src/EcoFOCIpy/io/wpak_parser.py
@@ -9,8 +9,6 @@ These include:
     fobj = src.get_data(filename, MooringID)
     Dataset = src.parse(fobj, **kwargs)
 """
-from io import BytesIO
-
 import pandas as pd
 
 
@@ -23,19 +21,24 @@ class wpak(object):
     """
 
     @staticmethod
-    def parse(filename=None, argos_file=False):
+    def parse(filename=None, argos_file=False, datetime_index=True):
         r"""
         Basic Method to open and read wpak csv files 
 
         Alternatively we can pass ARGOS retrieved data in 
 
+        Default pass the pandas dataframe back indexed by datetime
+
         returns pandas dataframe
         """
         assert filename != None , 'Must provide a datafile'
 
-        rawdata_df = pd.read_csv(filename, header=0, delimiter="s+")
+        rawdata_df = pd.read_csv(filename, header=0, delimiter="\s+")
         rawdata_df["date_time"] = pd.to_datetime(
             rawdata_df["DATE"] + " " + rawdata_df["TIME"], format="%y/%m/%d %H:%M:%S"
         )
+
+        if datetime_index:
+            rawdata_df = rawdata_df.set_index(pd.DatetimeIndex(rawdata_df['date_time'])).drop(['date_time','DATE','TIME'],axis=1)
 
         return rawdata_df
