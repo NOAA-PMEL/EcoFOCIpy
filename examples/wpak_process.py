c7b59fe91137193bc5a0c36765ad0f368703811c
shaunwbell <shaun.w.bell@gmail.com>
2021-03-26 11:30:15 -0700

    Wkpak build out (#13)
    * Add instrument yaml config
    
    * wpak intial analysis
    
    * Create lanzcos.py
    
    - 35 hour filter math for timeseries - see examples and jupyternotebooks for implementation
    
    * Update wpak_process.py
    
    * wpak preliminary creation
    
    - auto trim to dep/rec
    - filesave in foci naming conv
    - some global attributes
    
    * keep fleshing out wpak build
    
    * wrap up intial wpak code with example

--- examples/wpak_process.py
+++ examples/wpak_process.py
@@ -6,10 +6,92 @@ Demonstrate how to use the routines to process downloaded wpak data.
 
 Not valid for argos data (NRT - only)
 """
+import yaml
 
-from EcoFOCIpy import io.wpak_parser
+import src.EcoFOCIpy.io.wpak_parser as wpak_parser
+import src.EcoFOCIpy.io.ncCFsave as ncCFsave
+import src.EcoFOCIpy.metaconfig.load_config as load_config
 
-testfile = '..staticdata/wpak_test.txt'
-wpak = wpak_parser()
+###############################################################
+# edit to point to wpak raw datafile (arg parse?)
+datafile = 'staticdata/wpak_test.txt'
+instrument = 'Weatherpak 1361'
+mooring_meta_file = 'staticdata/mooring_example.yaml'
+inst_meta_file = 'src/EcoFOCIpy/metaconfig/wpak_epickeys.yaml'
+inst_shortname = 'wpak'
+###############################################################
 
-wpak.get_data(filename=testfile).parse()
+#init and load data
+wpak = wpak_parser.wpak()
+wpak_data = wpak.parse(filename=datafile,datetime_index=True) 
+
+# time resample (or decimate)
+### Choose one of the following two methods, or roll your own
+## interpolate subroutines
+"""
+absolute simplest time gridding
+ resample hourly will effectively truncate our times (only one data point in each hour)
+ interpolate will fill some gaps
+### like a time shift without adjusting data"""
+wpak_grid_simple = wpak_data.resample('1H').mean().interpolate(limit=3)
+
+"""
+slightly more complex method - valid when data is far enough from the hour
+ resample every 10min - but then linearly fill the supersampling
+ then decimate to the hour
+### very simple linear interpolation to hour"""
+wpak_grid_decimate = wpak_data.resample('10T').mean().interpolate(limit=6)
+wpak_grid_decimate = wpak_grid_decimate[wpak_grid_decimate.index.minute == 0]
+
+#TODO: Output CSV file for NRT erddap? end initial/nrt processing ? lat/lon?
+
+# Ingest instrumenttype parameter config file for meta information
+# undefined variables in the data may not make it past this point if not 
+#  given additional metainformation
+inst_config = load_config.load_config(inst_meta_file)
+
+# Ingest mooring yaml status file for deployment meta information
+#  This provides deployment specific details.  The deployment files are 
+#  created from the ecofoci-field database
+#TODO: migrate db->yaml tool into this package
+with open(mooring_meta_file) as file:
+    mooring_config = yaml.full_load(file)
+
+# Add meta data and prelim processing based on meta data
+# Convert to xarray and add meta information - save as CF netcdf file
+# pass -> data, instmeta, depmeta
+wpak_nc = ncCFsave.EcoFOCI_CFnc_moored(df=wpak_grid_decimate, 
+                                instrument_yaml=inst_config, 
+                                mooring_yaml=mooring_config, 
+                                instrument_id=instrument, 
+                                inst_shortname=inst_shortname)
+
+#add global attributes - not mandatory
+wpak_nc.deployment_meta_add()
+#add instituitonal global attributes
+wpak_nc.institution_meta_add()
+#add geospatial min/max data
+wpak_nc.temporal_geospatioal_meta_data()
+
+#add creation date/time - provenance data
+wpak_nc.provinance_meta_add()
+
+#provide intial qc status field
+wpak_nc.qc_status(qc_status='unknown')
+
+# combine trim (not mandatory) and filename together (saves to test.nc without name)
+wpak_nc.xarray2netcdf_save(xdf = wpak_nc.autotrim_time(),
+                           filename=wpak_nc.filename_const())
+
+### tests 
+
+def test_validate_mooringyaml_keys(yamlconfig):
+    for mand_key in ['MooringID', 'Deployment', 'Recovery', 'Notes', 'Instrumentation']:
+        assert mand_key in yamlconfig.keys(), f"{mand_key} not in Mooring Config file"
+
+def test_sample_data_size(data=wpak_data):
+    assert wpak_data.shape == (83, 13), 'test file not read in as expected - shape not right'
+
+def test_instrument_in_mooring(instrument='Weatherpak', mooring_config=mooring_config):
+    warning_text = 'Weatherpak not in mooring list, no meta info can be added'
+    assert instrument in [x for x in mooring_config['Instrumentation']], warning_text    
---
+++ src/EcoFOCIpy/io/ncCFsave.py
@@ -0,0 +1,118 @@
+"""Contains a collection of tools to save to netcdf.
+
+These include:
+
+* converting to xarray
+* adding global attributes
+* adding variable attributes
+
+"""
+import datetime
+
+import xarray as xr
+
+
+class EcoFOCI_CFnc_moored(object):
+    """Designed for moored instrumentation.
+
+    CTD data will use the seabird cnv package
+
+    Other platforms:
+        prawler
+        glider
+        saildrone
+
+    will use their own class
+    """
+
+    def __init__(self, df=None, instrument_yaml='', mooring_yaml=None, instrument_id='', inst_shortname=''):
+        """data is a pandas dataframe
+        Wich is immediatly converted to xarray
+        """
+        
+        self.xdf = df.to_xarray()
+        self.instrument_yaml = instrument_yaml
+        self.mooring_yaml = mooring_yaml
+        self.instrument_id = instrument_id
+        self.inst_shortname = inst_shortname
+
+    def institution_meta_add(self, institution_yaml=''):
+        pass
+
+    def deployment_meta_add(self):
+        """TODO: Validate content of times"""
+
+        attributes = {
+            'MooringID':self.mooring_yaml['MooringID'],
+            'platform_deployment_date':self.mooring_yaml['Deployment']['DeploymentDateTimeGMT'].strftime('%Y-%m-%dT%H:%M:%SZ'),
+            'platform_deployment_cruise_name': self.mooring_yaml['Deployment']['DeploymentCruise'],
+            'platform_recovery_date':self.mooring_yaml['Recovery']['RecoveryDateTimeGMT'].strftime('%Y-%m-%dT%H:%M:%SZ'),
+            'platform_recovery_cruise_name': self.mooring_yaml['Recovery']['RecoveryCruise'],
+            'platform_deployment_recovery_comments': self.mooring_yaml['Notes'],
+            'WaterDepth':self.mooring_yaml['Deployment']['DeploymentDepth']}
+
+        self.xdf.attrs.update(attributes)
+
+    def institution_meta_data(self):
+        """Add EcoFOCI base metadata"""
+        pass
+
+    def temporal_geospatioal_meta_data(self):
+        """Add min/max lat/lon/time bounds"""
+        attributes = {
+            'Latitude-Deg_MM.dd_W':self.mooring_yaml['Deployment']['DeploymentLatitude'],
+            'Longitude-Deg_MM.dd_N':self.mooring_yaml['Deployment']['DeploymentLongitude']}
+        self.xdf.attrs.update(attributes)
+
+    def provinance_meta_add(self):
+        """add creation time and placeholder for modified time"""
+        attributes = {
+            'date_created':datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ'),
+            'date_modified': ''}
+        self.xdf.attrs.update(attributes)
+
+    ### Break out following methods for modification to existing data
+    def history(self, history_text=''):
+        """TODO: prevent overwriting?"""
+
+        self.xdf.attrs.update({'history':history_text})
+
+    def qc_status(self,qc_status='unknown'):
+        """QC_indicator: Unknown, Excellent, ProbablyGood, Mixed"""
+        attributes = {'QC_indicator':qc_status}
+        self.xdf.attrs.update(attributes)
+
+    ###
+    def autotrim_time(self):
+        """using the recorded deployment/recovery records - trim time array
+        """
+        starttime = self.mooring_yaml['Deployment']['DeploymentDateTimeGMT']
+        endtime = self.mooring_yaml['Recovery']['RecoveryDateTimeGMT']
+        
+        try:   
+            self.history(history_text=self.xdf.attrs['history'] + '\nTrimmed to deployment.')
+        except:   
+            self.history(history_text='Trimmed to deployment.')
+
+        return self.xdf.sel(date_time=slice(starttime,endtime))
+
+
+    def filename_const(self):
+
+        #EcoFOCI standard mooring naming
+        #18bsm2a_wpak.nc - {mooringid}_{instshortname}_{depth}m.nc
+        try:
+            mooringID_simple = "".join(self.mooring_yaml['MooringID'].split('-')).lower()
+        except:
+            mooringID_simple = 'xxxxxx'
+        
+        try:
+            depth = str(int(self.mooring_confg['Instrumentation'][self.instrument_id]['ActualDepth'])).zfill(4)
+        except:
+            depth = '0000'
+
+        return( mooringID_simple+'_'+self.inst_shortname+'_'+depth+'m.nc' )
+
+    def xarray2netcdf_save(self, xdf=None, filename='temp.nc'):
+                
+        xdf.to_netcdf(filename)
