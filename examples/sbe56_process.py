e35d1edf4214994d33efecd09144a6498e4bbc4d
Shaun Bell <shaun.bell@noaa.gov>
2021-03-31 12:36:16 -0700

    add sbe16 parser
    - jupyter notebook example
    - parser source code
    - example dataset

---
+++ examples/sbe56_process.py
@@ -0,0 +1,98 @@
+"""
+sbe56 Example
+=============
+Demonstrate how to use the routines to process downloaded sbe56 cnv data
+Usable as a template for all other sbe-39 processing
+
+* with pressure and without pressure
+
+See Jupyter Notebook for commentary, walkthrough, and output
+"""
+import yaml
+
+import EcoFOCIpy.io.sbe_parser as sbe_parser
+import EcoFOCIpy.io.ncCFsave as ncCFsave
+import EcoFOCIpy.metaconfig.load_config as load_config
+
+###############################################################
+# edit to point to {instrument sepcific} raw datafile 
+datafile = sample_data_dir+'staticdata/example_data/sbe56_timeJ.cnv'
+instrument = 'SBE-56 2453'
+mooring_meta_file = sample_data_dir+'staticdata/mooring_example.yaml'
+inst_meta_file = sample_data_dir+'staticdata/instr_metaconfig/sbe56_cf.yaml'
+inst_shortname = 's56'
+###############################################################
+
+#init and load data
+sbe56_wop = sbe_parser.sbe56()
+(sbe56_wop_data,sbe56_wop_header) = sbe56_wop.parse(filename=datafile,
+                                                    return_header=True,
+                                                    datetime_index=True) 
+
+##### time frequency adjustment
+# this step can be done at any point and is usually a small shift for
+# most instruments
+#####
+
+#round off times to nearest sample frequency
+sbe56_wop_data.index = sbe56_wop_data.index.round(freq='10min')
+#resample to fix non-monotonic times (missing data) and fill linearly up to one hour
+sbe56_wop_data = sbe56_wop_data.resample('10min').mean().interpolate(limit=6)
+
+# Ingest instrumenttype parameter config file for meta information
+# undefined variables in the data may not make it past this point if not 
+#  given additional metainformation
+with open(inst_meta_file) as file:
+    inst_config = yaml.full_load(file)
+
+# Ingest mooring yaml status file for deployment meta information
+#  This provides deployment specific details.  The deployment files are 
+#  created from the ecofoci-field database
+#TODO: migrate db->yaml tool into this package
+with open(mooring_meta_file) as file:
+    mooring_config = yaml.full_load(file)
+
+# Add meta data and prelim processing based on meta data
+# Convert to xarray and add meta information - save as CF netcdf file
+# pass -> data, instmeta, depmeta
+### 1
+sbe56_wop_nc = ncCFsave.EcoFOCI_CFnc_moored(df=sbe56_wop_data, 
+                                instrument_yaml=inst_config, 
+                                mooring_yaml=mooring_config, 
+                                instrument_id=instrument, 
+                                inst_shortname=inst_shortname)
+
+#Following section is "optional" in the sense that not performing any of the steps will still
+# lead to a functional file, but just without much metadata... eg... (1,2) can be done to have a 
+# quicklook available... and temp config files can also be easily created in the same pattern
+# as the official ones.
+#--------------------------------------------------------------------------------------#
+# expand the dimensions and coordinate variables
+# renames them appropriatley and prepares them for meta-filled values
+sbe56_wop_nc.expand_dimensions()
+
+sbe56_wop_nc.variable_meta_data(variable_keys=['temperature'])
+sbe56_wop_nc.temporal_geospatioal_meta_data(depth='designed')
+#adding dimension meta needs to come after updating the dimension values... BUG?
+sbe56_wop_nc.dimension_meta_data(variable_keys=['depth','latitude','longitude'])
+
+#add global attributes
+sbe56_wop_nc.deployment_meta_add()
+sbe56_wop_nc.get_xdf()
+
+#add instituitonal global attributes
+sbe56_wop_nc.institution_meta_add()
+
+#add creation date/time - provenance data
+sbe56_wop_nc.provinance_meta_add()
+
+#provide intial qc status field
+sbe56_wop_nc.qc_status(qc_status='unknown')
+#--------------------------------------------------------------------------------------#
+
+### 2
+# combine trim (not mandatory) and filename together (saves to test.nc without name)
+sbe56_wop_nc.xarray2netcdf_save(xdf = sbe56_wop_nc.autotrim_time(),
+                           filename=sbe56_wop_nc.filename_const(depth='designed'),format="NETCDF3_CLASSIC")
+
+
--- src/EcoFOCIpy/io/sbe_parser.py
+++ src/EcoFOCIpy/io/sbe_parser.py
@@ -15,8 +15,112 @@ def sbetime_conversion(time_type='timeJ',data=None):
     """Seabird offers multiple time output options:
     timeJ:
     timeS:
+    timeJV2:
     """
     pass
+
+def seabird_header(filename=None):
+    r""" Seabird Instruments have a header usually defined by *END with a significant amount of
+    information imbedded.  Send a flag to parse seabird headers.  Better yet may be to combine seabird gear
+    into classes and subclasses.
+    """
+    assert filename.split('.')[-1] == 'cnv' , 'Must provide a tid file - use sbe software to convert'
+
+    header = []
+    var_names = {}
+    with open(filename) as fobj:
+        for k, line in enumerate(fobj.readlines()):
+            header = header + [line]
+            if "# name" in line:
+                var_names[int(line.split("=")[0].split()[-1])] = line.split("=")[1].split()[0].split(':')[0]
+            if "# start_time" in line:
+                start_time = line.split("[")[0].split("=")[-1].strip()
+            if "*END*" in line:
+                headercount=k+1
+                break
+
+    return (header, headercount, var_names, start_time)
+
+class sbe16(object):
+    r""" Seabird 16
+        
+        Basic Method to open files.  Specific actions can be passes as kwargs for instruments
+
+        There are quite a few instrument varations possible here - use the header meta to indentify variables
+
+    """
+
+
+    @staticmethod
+    def parse(filename=None, return_header=True, datetime_index=True):
+        r"""
+        Basic Method to open and read sbe16 .cnv files
+
+        """
+        assert filename.split('.')[-1] == 'cnv' , 'Must provide a cnv file - use sbe software to convert'
+
+        header = []
+        var_names = {}
+        with open(filename) as fobj:
+            for k, line in enumerate(fobj.readlines()):
+                header = header + [line]
+                if "# name" in line:
+                    var_names[int(line.split("=")[0].split()[-1])] = line.split("=")[1].split()[0].split(':')[0]
+                if "# start_time" in line:
+                    start_time = line.split("[")[0].split("=")[-1].strip()
+                if "*END*" in line:
+                    headercount=k+1
+                    break
+
+
+        rawdata_df = pd.read_csv(filename, 
+                        delimiter="\s+", 
+                        parse_dates=True, 
+                        header=None,
+                        names=var_names.values(), 
+                        skiprows=headercount)
+
+        if 'timeJ' in var_names.values():
+            rawdata_df['date_time'] = [datetime.datetime.strptime(start_time, "%b %d %Y %H:%M:%S") + pd.Timedelta(days=x) for x in rawdata_df['timeJ']]
+        elif 'timeJV2' in var_names.values():
+            rawdata_df['date_time'] = [datetime.datetime.strptime(start_time, "%b %d %Y %H:%M:%S") + pd.Timedelta(days=x) for x in rawdata_df['timeJV2']]
+        elif 'timeS' in var_names.values():
+            rawdata_df['date_time'] = [datetime.datetime.strptime(start_time, "%b %d %Y %H:%M:%S") + pd.Timedelta(seconds=x) for x in rawdata_df['timeS']]
+        else:
+            print(f'no time index identified: {var_names.values()}')
+
+        if datetime_index:
+            rawdata_df = rawdata_df.set_index(pd.DatetimeIndex(rawdata_df['date_time'])).drop(['date_time'],axis=1)        
+
+        return (rawdata_df,header)
+
+class sbe26(object):
+    r""" Seabird 26 Wave and Tide GuageTemperature (with optional pressure)
+        
+        Basic Method to open files.  Specific actions can be passes as kwargs for instruments
+
+    """
+
+    @staticmethod
+    def parse(filename=None, datetime_index=True):
+        r"""
+        Basic Method to open and read sbe26 .tid files
+
+        """
+        assert filename.split('.')[-1] == 'tid' , 'Must provide a tid file - use sbe software to convert'
+
+        rawdata_df = pd.read_csv(filename, 
+                                 delimiter="\s+", 
+                                 skiprows=1, 
+                                 names=["date", "time", "pressure", "temperature"])
+        
+        
+        rawdata_df["date_time"] = pd.to_datetime(rawdata_df["date"] + " " + rawdata_df["time"], format="%m/%d/%Y %H:%M:%S")
+
+        if datetime_index:
+            rawdata_df = rawdata_df.set_index(pd.DatetimeIndex(rawdata_df['date_time'])).drop(['date_time','date','time'],axis=1)        
+
+        return (rawdata_df)
 
 # Temp, Press, Sal, Cond
 class sbe37(object):
