676d7be921ff794ad7421c1972b76239e66780af
shaunwbell <shaun.w.bell@gmail.com>
2021-04-06 11:26:40 -0700

    Cruise meta creator (#40)
    * update castlog meta creation
    
    * Update cruise_config_creator.py

--- tools/cruise_config_creator.py
+++ tools/cruise_config_creator.py
@@ -54,20 +54,30 @@ EcoFOCI_db = EcoFOCI_db_datastatus()
 (db,cursor) = EcoFOCI_db.connect_to_DB(db_config_file=config_file)
 
 table = 'cruises'
-Cruise_Meta_sum = EcoFOCI_db.read_mooring_summary(table=table, cruiseid=args['CruiseID'], verbose=True)
+Cruise_Meta_sum = EcoFOCI_db.read_cruise_summary(table=table, cruiseid=args['CruiseID'], verbose=True)
 table = 'cruisecastlogs'
-Cruise_Castlogs_sum = EcoFOCI_db.read_mooring_summary(table=table, cruiseid=args['CruiseID'])
+Cruise_Castlogs_sum = EcoFOCI_db.read_castlog_summary(table=table, cruiseid=args['CruiseID'])
 
 EcoFOCI_db.close()
 
-try:
-    Cruise_Meta_sum[args['CruiseID']]
-except:
-    print("No known cruise {0}.  Check syntax and case (e.g. dy1908)".format(args['CruiseID']))
-    sys.exit()
-    
 
 if args.yaml_format:
-    data_dic = {'CruiseID':None, 'Deployment':None, 'Recovery':None, 'Notes':None, 'Instrumentation':None}
-    data_dic['CruiseID'] = args['CruiseID']
+    data_dic = {}
+    try:
+        data_dic.update({"Cruise":Cruise_Meta_sum})
+    except:
+        print("No known cruise {0}.  Check syntax and case (e.g. dy1908)".format(args['CruiseID']))
+        sys.exit()
+    try:
+        #build a dictionary of dictionaries for ctd casts
+        CTDDic = {}
+        for profile in sorted(Cruise_Castlogs_sum.keys()):
+            CTDDic[Cruise_Castlogs_sum[profile]['ConsecutiveCastNumber']] = Cruise_Castlogs_sum[profile]
+            
+        data_dic.update({"CTDCasts":CTDDic})
+    except:
+        print("An issue exists in the CTDcasts records")
+        sys.exit()        
 
+    
+    load_config.write_config(args.CruiseID+'.yaml', data_dic)
