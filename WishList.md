32422890c3164c49155c16da8854cd73e1193875
shaunwbell <shaun.w.bell@gmail.com>
2021-08-03 08:54:18 -0700

    Notebooks and updates (#109)
    * update notebooks
    
    * add wishlist and bottleneck dep
    
    * add filter techniques
    
    * Create TimeSeriesStickPlot.py
    
    * spelling issues
    
    * Create 16bsm2a_eco_0000m.nc

---
+++ WishList.md
@@ -0,0 +1,17 @@
+# Wish list of features and links to good plans
+
+## Erddap related
+- aggregating features for common FOCI searches (long term moorings: M2, M4, M5, M8, C2 - usually biophysical and dynamical)
+
+## Products
+- near surface temperatures
+- near bottom temperatures
+
+## ??
+- gridded full timeseries at sites (in addition to deployment by deployment)
+- full cruise season CTD collections?
+- Vessel CTD collections
+
+## Archived datasets and download
+
+### Automated Tasks to generate products above
---
+++ src/EcoFOCIpy/plots/TimeSeriesStickPlot.py
@@ -0,0 +1,148 @@
+
+"""
+EcoFOCI Cruise Seabird QuickLook
+
+Plots data from csv and cnv files
+"""
+import datetime
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import numpy as np
+from matplotlib.dates import (DateFormatter, DayLocator, HourLocator,
+                              MonthLocator, WeekdayLocator, YearLocator)
+
+__all__ = ["Timeseries1dStickPlot"]
+
+class Timeseries1dStickPlot(object):
+
+    mpl.rcParams['svg.fonttype'] = 'none'
+    mpl.rcParams['ps.fonttype'] = 42
+    mpl.rcParams['pdf.fonttype'] = 42
+    
+    def __init__(self, fontsize=10, labelsize=10, plotstyle='k-.', stylesheet='bmh'):
+        """Initialize the timeseries with items that do not change.
+
+        This sets up the axes and station locations. The `fontsize` and `spacing`
+        are also specified here to ensure that they are consistent between individual
+        station elements.
+
+        Parameters
+        ----------
+        fontsize : int
+            The fontsize to use for drawing text
+        labelsize : int
+          The fontsize to use for labels
+        stylesheet : str
+          Choose a mpl stylesheet [u'seaborn-darkgrid', 
+          u'seaborn-notebook', u'classic', u'seaborn-ticks', 
+          u'grayscale', u'bmh', u'seaborn-talk', u'dark_background', 
+          u'ggplot', u'fivethirtyeight', u'seaborn-colorblind', 
+          u'seaborn-deep', u'seaborn-whitegrid', u'seaborn-bright', 
+          u'seaborn-poster', u'seaborn-muted', u'seaborn-paper', 
+          u'seaborn-white', u'seaborn-pastel', u'seaborn-dark', 
+          u'seaborn-dark-palette']
+        """
+
+        self.fontsize = fontsize
+        self.labelsize = labelsize
+        self.plotstyle = plotstyle
+        plt.style.use(stylesheet)
+
+    @staticmethod
+    def add_title(mooringid='',lat=-99.9,lon=-99.9,depth=9999,instrument=''):
+      """Pass parameters to annotate the title of the plot
+
+      This sets the standard plot title using common meta information from PMEL/EPIC style netcdf files
+
+      Parameters
+      ----------
+      mooringid : str
+        Mooring Identifier
+      lat : float
+        The latitude of the mooring
+      lon : float
+        The longitude of the mooring
+      depth : int
+        Nominal depth of the instrument
+      instrument : str
+        Name/identifier of the instrument plotted
+      """  
+      ptitle = ("Plotted on: {time:%Y/%m/%d %H:%M} \n from {mooringid} Lat: {latitude:3.3f}  Lon: {longitude:3.3f}" 
+            " Depth: {depth}\n : {instrument}").format(
+            time=datetime.datetime.now(), 
+                  mooringid=mooringid,
+                  latitude=lat, 
+                  longitude=lon, 
+                  depth=depth,
+                  instrument=instrument )
+
+      return ptitle
+
+
+    def plot(self, timedata=None, udata=None, vdata=None, ylabel=None, linescale=1, **kwargs):
+
+      if kwargs['rotate'] != 0.0:
+          #when rotating vectors - positive(+) rotation is equal to cw of the axis (ccw of vector)
+          #                      - negative(+) rotation is equal to ccw of the axis (cw of the vector)
+          print("rotating vectors")
+          angle_offset_rad = np.deg2rad(kwargs['rotate'])
+          udata = udata*np.cos(angle_offset_rad) + vdata*np.sin(angle_offset_rad)
+          vdata = -1.*udata*np.sin(angle_offset_rad) + vdata*np.cos(angle_offset_rad)
+
+      magnitude = np.sqrt(udata**2 + vdata**2)
+
+      fig, (ax1,ax2) = plt.subplots(2,1,sharex='col',figsize=(11,4.25))
+
+
+      # Plot u and v components
+      # Plot quiver
+      ax1.set_ylim(-1*np.nanmax(magnitude), np.nanmax(magnitude))
+      fill1 = ax1.fill_between(timedata, magnitude, 0, color='k', alpha=0.1)
+
+      # Fake 'box' to be able to insert a legend for 'Magnitude'
+      """
+      p = ax1.add_patch(plt.Rectangle((1,1),1,1,fc='k',alpha=0.1))
+      leg1 = ax1.legend([p], ["Current magnitude [cm/s]"],loc='lower right')
+      leg1._drawFrame=False
+      """
+
+      # 1D Quiver plot
+      q = ax1.quiver(timedata,0,udata,vdata,color='r',units='y',scale_units='y',
+                     scale = 1,headlength=1,headaxislength=1,width=0.04*linescale,alpha=.95)
+      qk = plt.quiverkey(q,0.2, 0.05, 5,r'$5 \frac{cm}{s}$',labelpos='W',
+                     fontproperties={'weight': 'bold'})
+
+
+      # Plot u and v components
+      ax1.set_xticklabels(ax1.get_xticklabels(), visible=False)
+      ax2.set_xticklabels(ax2.get_xticklabels(), visible=True)
+      ax1.axes.get_xaxis().set_visible(False)
+      ax1.set_xlim(timedata.min(),timedata.max())
+      ax1.set_ylabel("Velocity (cm/s)")
+      ax2.plot(timedata, vdata, 'b-', linewidth=0.25)
+      ax2.plot(timedata, udata, 'g-', linewidth=0.25)
+      ax2.set_xlim(timedata.min(),timedata.max())
+      ax2.set_xlabel("Date (UTC)")
+      ax2.set_ylabel("Velocity (cm/s)")
+      ax2.xaxis.set_major_locator(MonthLocator())
+      ax2.xaxis.set_minor_locator(MonthLocator(bymonth=range(1,13), bymonthday=15))
+      ax2.xaxis.set_major_formatter(ticker.NullFormatter())
+      ax2.xaxis.set_minor_formatter(DateFormatter('%b %y'))
+      ax1.spines['bottom'].set_visible(False)
+      ax2.spines['top'].set_visible(False)
+      ax1.xaxis.set_ticks_position('top')
+      ax2.xaxis.set_ticks_position('bottom')
+      ax2.yaxis.set_ticks_position('both')
+      ax2.tick_params(axis='both', which='minor', labelsize=self.labelsize)
+      ax1.tick_params(axis='both', which='minor', labelsize=self.labelsize)
+      #manual time limit sets
+      #ax1.set_xlim([datetime.datetime(2016,2,1),datetime.datetime(2016,9,15)])
+      #ax2.set_xlim([datetime.datetime(2016,2,1),datetime.datetime(2016,9,15)])
+      # Set legend location - See: http://matplotlib.org/Volumes/WDC_internal/users/legend_guide.html#legend-location
+      leg2 = plt.legend(['v','u'],loc='upper left')
+      leg2._drawFrame=False
+
+
+      return plt, fig
