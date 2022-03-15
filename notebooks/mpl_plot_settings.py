65eb4180229a4d931a936894152da5f8b3a12d8a
Shaun Bell <shaun.bell@noaa.gov>
2022-03-15 08:37:05 -0700

    Create mpl_plot_settings.py
    import this in front of plot routines in jupyter-lab to get consistent plot style

---
+++ notebooks/mpl_plot_settings.py
@@ -0,0 +1,31 @@
+#small file to import to 
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+### specify primary bulk figure parameters
+fontsize = 10
+labelsize = 10
+#plotstyle = 'seaborn'
+max_xticks = 10
+plt.style.use('seaborn-ticks')
+mpl.rcParams['svg.fonttype'] = 'none'
+mpl.rcParams['ps.fonttype'] = 42 #truetype/type2 fonts instead of type3
+mpl.rcParams['pdf.fonttype'] = 42 #truetype/type2 fonts instead of type3
+mpl.rcParams['axes.grid'] = False
+mpl.rcParams['axes.edgecolor'] = 'black'
+mpl.rcParams['axes.linewidth'] = 1.5
+mpl.rcParams['axes.labelcolor'] = 'black'
+mpl.rcParams['grid.linestyle'] = '--'
+mpl.rcParams['grid.linestyle'] = '--'
+mpl.rcParams['xtick.major.size'] = 4
+mpl.rcParams['xtick.minor.size'] = 2
+mpl.rcParams['xtick.major.width'] = 2
+mpl.rcParams['xtick.minor.width'] = 0.5
+mpl.rcParams['ytick.major.size'] = 4
+mpl.rcParams['ytick.minor.size'] = 2
+mpl.rcParams['ytick.major.width'] = 2
+mpl.rcParams['ytick.minor.width'] = 0.5
+mpl.rcParams['ytick.direction'] = 'out'
+mpl.rcParams['xtick.direction'] = 'out'
+mpl.rcParams['ytick.color'] = 'black'
+mpl.rcParams['xtick.color'] = 'black'
