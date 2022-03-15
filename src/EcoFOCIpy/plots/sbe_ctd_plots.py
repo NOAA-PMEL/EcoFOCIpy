5e077b01a516598a72716805eb79f872db5a2287
Shaun Bell <shaun.bell@noaa.gov>
2022-03-15 08:37:46 -0700

    Update sbe_ctd_plots.py
    3 var plot fix

--- src/EcoFOCIpy/plots/sbe_ctd_plots.py
+++ src/EcoFOCIpy/plots/sbe_ctd_plots.py
@@ -180,144 +180,144 @@ class CTDProfilePlot(object):
         return plt, fig    
 
     def plot3var(self, varname=None, xdata=None, ydata=None, xlabel=None, secondary=False, **kwargs):
-      fig = plt.figure(1)
-      ax1 = fig.add_subplot(111)
-      p1 = ax1.plot(xdata[0], ydata)
-      plt.setp(p1, color=self.var2format(varname[0])['color'],
-                  linestyle=self.var2format(varname[0])['linestyle'],
-                  linewidth=self.var2format(varname[0])['linewidth'])
-      if secondary and not (xdata[1].size == 0):
-        p1 = ax1.plot(xdata[1],ydata)
-        plt.setp(p1, color=self.var2format(varname[1])['color'],
-                    linestyle=self.var2format(varname[1])['linestyle'],
-                    linewidth=self.var2format(varname[1])['linewidth'])
-        #set plot limits for two vars by finding the absolute range and adding 10%
-        abmin=np.nanmin([np.nanmin(xdata[0]),np.nanmin(xdata[1])])
-        abmax=np.nanmax([np.nanmax(xdata[0]),np.nanmax(xdata[1])])
-        ax1.set_xlim([abmin - 0.1*(abmax-abmin),abmax + 0.1*(abmax-abmin)])
+        fig = plt.figure(1)
+        ax1 = fig.add_subplot(111)
+        p1 = ax1.plot(xdata[0], ydata)
+        plt.setp(p1, color=self.var2format(varname[0])['color'],
+                    linestyle=self.var2format(varname[0])['linestyle'],
+                    linewidth=self.var2format(varname[0])['linewidth'])
+        if secondary and not (xdata[1].size == 0):
+          p1 = ax1.plot(xdata[1],ydata)
+          plt.setp(p1, color=self.var2format(varname[1])['color'],
+                      linestyle=self.var2format(varname[1])['linestyle'],
+                      linewidth=self.var2format(varname[1])['linewidth'])
+          #set plot limits for two vars by finding the absolute range and adding 10%
+          abmin=np.nanmin([np.nanmin(xdata[0]),np.nanmin(xdata[1])])
+          abmax=np.nanmax([np.nanmax(xdata[0]),np.nanmax(xdata[1])])
+          ax1.set_xlim([abmin - 0.1*(abmax-abmin),abmax + 0.1*(abmax-abmin)])
 
-      ax1.invert_yaxis()
-      plt.ylabel('Depth (dB)', fontsize=self.labelsize, fontweight='bold')
-      plt.xlabel(xlabel[0], fontsize=self.labelsize, fontweight='bold')
-    
-      fmt=mpl.ticker.StrMethodFormatter(self.var2format(varname[0])['format'])
-      ax1.xaxis.set_major_formatter(fmt)
-      ax1.tick_params(axis='both', which='major', labelsize=self.labelsize)
+        ax1.invert_yaxis()
+        plt.ylabel('Depth (dB)', fontsize=self.labelsize, fontweight='bold')
+        plt.xlabel(xlabel[0], fontsize=self.labelsize, fontweight='bold')
+      
+        fmt=mpl.ticker.StrMethodFormatter(self.var2format(varname[0])['format'])
+        ax1.xaxis.set_major_formatter(fmt)
+        ax1.tick_params(axis='both', which='major', labelsize=self.labelsize)
 
-      #plot second param
-      ax2 = ax1.twiny()
-      p1 = ax2.plot(xdata[2], ydata)
-      plt.setp(p1, color=self.var2format(varname[2])['color'],
-                  linestyle=self.var2format(varname[2])['linestyle'],
-                  linewidth=self.var2format(varname[2])['linewidth'])
-      if secondary and not (xdata[3].size == 0):
-        p1 = ax2.plot(xdata[3],ydata)
-        plt.setp(p1, color=self.var2format(varname[3])['color'],
-                    linestyle=self.var2format(varname[3])['linestyle'],
-                    linewidth=self.var2format(varname[3])['linewidth'])
-        #set plot limits for two vars by finding the absolute range and adding 10%
-        abmin=np.nanmin([np.nanmin(xdata[2]),np.nanmin(xdata[3])])
-        abmax=np.nanmax([np.nanmax(xdata[2]),np.nanmax(xdata[3])])
-        try:
-          ax2.set_xlim([abmin - 0.1*(abmax-abmin),abmax + 0.1*(abmax-abmin)])
-        except:
-          ax2.set_xlim([0,1])
+        #plot second param
+        ax2 = ax1.twiny()
+        p1 = ax2.plot(xdata[2], ydata)
+        plt.setp(p1, color=self.var2format(varname[2])['color'],
+                    linestyle=self.var2format(varname[2])['linestyle'],
+                    linewidth=self.var2format(varname[2])['linewidth'])
+        if secondary and not (xdata[3].size == 0):
+          p1 = ax2.plot(xdata[3],ydata)
+          plt.setp(p1, color=self.var2format(varname[3])['color'],
+                      linestyle=self.var2format(varname[3])['linestyle'],
+                      linewidth=self.var2format(varname[3])['linewidth'])
+          #set plot limits for two vars by finding the absolute range and adding 10%
+          abmin=np.nanmin([np.nanmin(xdata[2]),np.nanmin(xdata[3])])
+          abmax=np.nanmax([np.nanmax(xdata[2]),np.nanmax(xdata[3])])
+          try:
+            ax2.set_xlim([abmin - 0.1*(abmax-abmin),abmax + 0.1*(abmax-abmin)])
+          except:
+            ax2.set_xlim([0,1])
 
-      plt.ylabel('Depth (dB)', fontsize=self.labelsize, fontweight='bold')
-      plt.xlabel(xlabel[1], fontsize=self.labelsize, fontweight='bold')
+        plt.ylabel('Depth (dB)', fontsize=self.labelsize, fontweight='bold')
+        plt.xlabel(xlabel[1], fontsize=self.labelsize, fontweight='bold')
 
-      fmt=mpl.ticker.StrMethodFormatter(self.var2format(varname[2])['format'])
-      ax2.xaxis.set_major_formatter(fmt)
-      ax2.tick_params(axis='x', which='major', labelsize=self.labelsize)
+        fmt=mpl.ticker.StrMethodFormatter(self.var2format(varname[2])['format'])
+        ax2.xaxis.set_major_formatter(fmt)
+        ax2.tick_params(axis='x', which='major', labelsize=self.labelsize)
 
-      ax3 = ax1.twiny()
-      ax3.spines["top"].set_position(("axes", 1.05))
-      self.make_patch_spines_invisible(ax3)
-      # Second, show the right spine.
-      ax3.spines["top"].set_visible(True)
-      p1 = ax3.plot(xdata[4], ydata)
-      plt.setp(p1, color=self.var2format(varname[4])['color'],
-                  linestyle=self.var2format(varname[4])['linestyle'],
-                  linewidth=self.var2format(varname[4])['linewidth'])
-      if secondary and not (xdata[5].size == 0):
-        p1 = ax2.plot(xdata[5],ydata)
-        plt.setp(p1, color=self.var2format(varname[5])['color'],
-                    linestyle=self.var2format(varname[5])['linestyle'],
-                    linewidth=self.var2format(varname[5])['linewidth'])
-        #set plot limits for two vars by finding the absolute range and adding 10%
-        abmin=np.nanmin([np.nanmin(xdata[4]),np.nanmin(xdata[5])])
-        abmax=np.nanmax([np.nanmax(xdata[4]),np.nanmax(xdata[5])])
-        ax3.set_xlim([abmin - 0.1*(abmax-abmin),abmax + 0.1*(abmax-abmin)])
+        ax3 = ax1.twiny()
+        ax3.spines["top"].set_position(("axes", 1.05))
+        self.make_patch_spines_invisible(ax3)
+        # Second, show the right spine.
+        ax3.spines["top"].set_visible(True)
+        p1 = ax3.plot(xdata[4], ydata)
+        plt.setp(p1, color=self.var2format(varname[4])['color'],
+                    linestyle=self.var2format(varname[4])['linestyle'],
+                    linewidth=self.var2format(varname[4])['linewidth'])
+        if secondary and not (xdata[5].size == 0):
+          p1 = ax2.plot(xdata[5],ydata)
+          plt.setp(p1, color=self.var2format(varname[5])['color'],
+                      linestyle=self.var2format(varname[5])['linestyle'],
+                      linewidth=self.var2format(varname[5])['linewidth'])
+          #set plot limits for two vars by finding the absolute range and adding 10%
+          abmin=np.nanmin([np.nanmin(xdata[4]),np.nanmin(xdata[5])])
+          abmax=np.nanmax([np.nanmax(xdata[4]),np.nanmax(xdata[5])])
+          ax3.set_xlim([abmin - 0.1*(abmax-abmin),abmax + 0.1*(abmax-abmin)])
 
-      plt.ylabel('Depth (dB)', fontsize=self.labelsize, fontweight='bold')
-      plt.xlabel(xlabel[2], fontsize=self.labelsize, fontweight='bold')
+        plt.ylabel('Depth (dB)', fontsize=self.labelsize, fontweight='bold')
+        plt.xlabel(xlabel[2], fontsize=self.labelsize, fontweight='bold')
 
-      #set bounds based on max and min values
+        #set bounds based on max and min values
 
-      #set xticks and labels to be at the same spot for all three vars
-      ax1.set_xticks(np.linspace(ax1.get_xbound()[0], ax1.get_xbound()[1], self.max_xticks))
-      ax2.set_xticks(np.linspace(ax2.get_xbound()[0], ax2.get_xbound()[1], self.max_xticks))
-      ax3.set_xticks(np.linspace(ax3.get_xbound()[0], ax3.get_xbound()[1], self.max_xticks))
+        #set xticks and labels to be at the same spot for all three vars
+        ax1.set_xticks(np.linspace(ax1.get_xbound()[0], ax1.get_xbound()[1], self.max_xticks))
+        ax2.set_xticks(np.linspace(ax2.get_xbound()[0], ax2.get_xbound()[1], self.max_xticks))
+        ax3.set_xticks(np.linspace(ax3.get_xbound()[0], ax3.get_xbound()[1], self.max_xticks))
 
-      fmt=mpl.ticker.StrMethodFormatter(self.var2format(varname[4])['format'])
-      ax3.xaxis.set_major_formatter(fmt)
-      ax3.tick_params(axis='x', which='major', labelsize=self.labelsize)
+        fmt=mpl.ticker.StrMethodFormatter(self.var2format(varname[4])['format'])
+        ax3.xaxis.set_major_formatter(fmt)
+        ax3.tick_params(axis='x', which='major', labelsize=self.labelsize)
 
-      return plt, fig
+        return plt, fig
 
     @staticmethod
     def var2format(varname):
-      """list of plot specifics based on variable name"""
-      plotdic={}
-      if varname in ['temperature_ch1']:
-        plotdic['color']='red'
-        plotdic['linestyle']='-'
-        plotdic['linewidth']=0.5
-        plotdic['format']='{x:.3f}'
-      elif varname in ['temperature_ch2']:
-        plotdic['color']='magenta'
-        plotdic['linestyle']='--'
-        plotdic['linewidth']=0.5
-        plotdic['format']='{x:.3f}'
-      elif varname in ['salinity_ch1', 'oxy_percentsat_ch1', 'oxy_conc_ch1']:
-        plotdic['color']='blue'
-        plotdic['linestyle']='-'
-        plotdic['linewidth']=0.5
-        if varname in ['salinity_ch1']:
+        """list of plot specifics based on variable name"""
+        plotdic={}
+        if varname in ['temperature_ch1']:
+          plotdic['color']='red'
+          plotdic['linestyle']='-'
+          plotdic['linewidth']=0.5
           plotdic['format']='{x:.3f}'
-        else:
-          plotdic['format']='{x:3.1f}'
-      elif varname in ['salinity_ch2', 'oxy_percentsat_ch2', 'oxy_conc_ch2']:
-        plotdic['color']='cyan'
-        plotdic['linestyle']='--'
-        plotdic['linewidth']=0.5
-        plotdic['format']='{x:3.1f}'
-        if varname in ['salinity_ch2']:
+        elif varname in ['temperature_ch2']:
+          plotdic['color']='magenta'
+          plotdic['linestyle']='--'
+          plotdic['linewidth']=0.5
           plotdic['format']='{x:.3f}'
-        else:
+        elif varname in ['salinity_ch1', 'oxy_percentsat_ch1', 'oxy_conc_ch1']:
+          plotdic['color']='blue'
+          plotdic['linestyle']='-'
+          plotdic['linewidth']=0.5
+          if varname in ['salinity_ch1']:
+            plotdic['format']='{x:.3f}'
+          else:
+            plotdic['format']='{x:3.1f}'
+        elif varname in ['salinity_ch2', 'oxy_percentsat_ch2', 'oxy_conc_ch2']:
+          plotdic['color']='cyan'
+          plotdic['linestyle']='--'
+          plotdic['linewidth']=0.5
           plotdic['format']='{x:3.1f}'
-      elif varname in ['sigmatheta','turbidity','sigmaT']:
-        plotdic['color']='black'
-        plotdic['linestyle']='-'
-        plotdic['linewidth']=0.5
-        plotdic['format']='{x:.3f}'
-      elif varname in ['chlor_fluorescence']:
-        plotdic['color']='green'
-        plotdic['linestyle']='-'
-        plotdic['linewidth']=0.5
-        plotdic['format']='{x:.2f}'
-      elif varname in ['par']:
-        plotdic['color']='darkorange'
-        plotdic['linestyle']='-'
-        plotdic['linewidth']=0.75
-        plotdic['format']='{x:5.0f}'
-      else:
-        plotdic['color']='black'
-        plotdic['linestyle']='--'
-        plotdic['linewidth']=1.0      
-        plotdic['format']='{x:.3f}'
+          if varname in ['salinity_ch2']:
+            plotdic['format']='{x:.3f}'
+          else:
+            plotdic['format']='{x:3.1f}'
+        elif varname in ['sigmatheta','turbidity','sigmaT']:
+          plotdic['color']='black'
+          plotdic['linestyle']='-'
+          plotdic['linewidth']=0.5
+          plotdic['format']='{x:.3f}'
+        elif varname in ['chlor_fluorescence']:
+          plotdic['color']='green'
+          plotdic['linestyle']='-'
+          plotdic['linewidth']=0.5
+          plotdic['format']='{x:.2f}'
+        elif varname in ['par']:
+          plotdic['color']='darkorange'
+          plotdic['linestyle']='-'
+          plotdic['linewidth']=0.75
+          plotdic['format']='{x:5.0f}'
+        else:
+          plotdic['color']='black'
+          plotdic['linestyle']='--'
+          plotdic['linewidth']=1.0      
+          plotdic['format']='{x:.3f}'
 
-      return plotdic
+        return plotdic
 
     @staticmethod
     #python3 change as dictionaries no longer have itervalues methods
