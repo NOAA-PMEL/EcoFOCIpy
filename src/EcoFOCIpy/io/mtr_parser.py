757c455fe92ab0742ea59ee4c0ed3131e6cec09d
Shaun Bell <shaun.bell@noaa.gov>
2022-06-14 08:44:52 -0700

    Merge branch 'main' of https://github.com/NOAA-PMEL/EcoFOCIpy

--- src/EcoFOCIpy/io/mtr_parser.py
+++ src/EcoFOCIpy/io/mtr_parser.py
@@ -6,6 +6,8 @@ These include:
 * Version 5 (MTRduino) [x]
 
 """
+import datetime
+import numpy as np
 import pandas as pd
 
 
@@ -19,14 +21,10 @@ class mtrduino(object):
     ToDO:  Allow raw data to be passed"""
 
     @staticmethod
-    def parse(filename=None, datetime_index=True, **kwargs):
+    def parse(filename=None, datetime_index=True):
         r"""
         Basic Method to open and read mtrduino raw converted csv files
             
-            kwargs:
-                round_10min_interval - force small deviations to 10min intervals.  Actually rounds all so it
-                    is user responisbility to make sure values are representative first.
-
         """
         assert filename != None , 'Must provide a datafile'
 
@@ -38,3 +36,187 @@ class mtrduino(object):
             rawdata_df = rawdata_df.set_index(pd.DatetimeIndex(rawdata_df['date_time'])).drop(['date_time'],axis=1)
 
         return rawdata_df
+
+class mtr(object):
+    r""" MicroTemperature Recorders (MTR)
+    Collection of static methods to define MTR processing and conversion"""
+
+    @staticmethod
+    def parse(filename=None, return_header=True, datetime_index=True, version=4.0):
+        r"""Parse MTR Data
+
+            kwargs
+            mtr_coef : list
+            [CoefA, CoefB, CoefC]
+
+        """
+        assert filename != None , 'Must provide a datafile'
+
+        skiprows = 0
+        hexlines = {}
+        header = []
+
+        fobj = open(filename)
+
+        for k, line in enumerate(fobj.readlines()):
+            line = line.strip()
+            if skiprows == 0:
+                header = header + [line]
+
+            if "READ" in line:  # Get end of header.
+                skiprows = k
+
+            if ("CMD" in line) and (k > skiprows) and (skiprows != 0) and (version != 4.1):  # Get end of file.
+                break
+            elif ("mtr>" in line) and (k > skiprows) and (skiprows != 0) and (version == 4.1):
+                break
+
+            if (k > skiprows) and (skiprows != 0):
+                hexlines[k] = line
+
+        if return_header:
+            return(hexlines, header)
+        else:
+            return hexlines
+
+        # ### create time and data streams
+        # count = 0
+        # time = {}
+        # temp = {}
+        # deltat = 0  # 10 min usually
+        # for sam_num in data_dic:
+        #     time[count] = declines[sam_num]["time"]
+        #     try:
+        #         deltat = (
+        #             declines[count + 1]["time"] - declines[count]["time"]
+        #         )  # in seconds
+        #         deltat = deltat / 120  # number of samples per record and convert
+        #     except:
+        #         datetime.timedelta(0)
+        #     # loop through dictionary rows
+        #     for i_row in range(0, 10, 1):  # loop through rows
+        #         for i_col in range(0, 12, 1):
+        #             temp[count] = declines[sam_num]["resistance_" + str(i_row)][i_col]
+        #             count += 1
+        #             time[count] = time[count - 1] + deltat
+        # time.pop(
+        #     time.keys()[-1]
+        # )  # delta function adds an extra step so remove last entry
+
+    @staticmethod
+    def hex2dec(data_dic, model_factor=4.0e08):
+        """
+        model factor parameter is based on serial number range 
+        for counts to resistance conversion
+        if (args.SerialNo / 1000 == 3) or (args.SerialNo / 1000 == 4):
+            model_factor = 4.0e+08
+        """
+        sample_num = 0
+        data = {}
+        for k, v in data_dic.items():
+            if len(v) == 16:  # timeword mmddyyhhmmssxxxx
+                data[sample_num] = {
+                    "time": datetime.datetime.strptime(v[:-4], "%m%d%y%H%M%S")
+                }
+                sample_num += 1
+                start_data = 0
+            elif len(v) == 4:  # checksum
+                continue
+            elif len(v) == 48:
+                # resistance values - 4char hex, 12 measurements, 10 consecutive lines
+                # break string into chunks with 4char
+                count = 4
+                row = [
+                    "".join(x) for x in zip(*[list(v[z::count]) for z in range(count)])
+                ]
+                # convert to decimal
+                row = [int(x, 16) for x in row]
+                data[sample_num - 1]["resistance_" + str(start_data)] = [
+                    (model_factor / x) if x != 0 else 0 for x in row
+                ]
+                start_data += 1
+            else:
+                pass
+                # periodically, it is known that a measurement gets dropped and the line needs
+                # to be filled to 48 characters
+
+        return data
+
+    def res2temp(self, data_dic, mtr_coef=[0,0,0]):
+        """Wrapper around steinhart_hart equation to convert resistance into temperature
+
+        Args:
+            data_dic (_type_): MTR Data Dictionary
+            mtr_coef (list, optional): _description_. Defaults to [0,0,0].
+
+        Returns:
+            _type_: _description_
+        """
+        
+        for sam_num in data_dic:
+            for k, v in data_dic[sam_num].items():
+                if not k == "time":
+                    data_dic[sam_num][k] = [
+                        self.steinhart_hart(x, mtr_coef)
+                        for x in data_dic[sam_num][k]
+                    ]
+        return data_dic
+
+    def steinhart_hart(self, resistance, mtr_coef=[0,0,0]):
+        """Convert resistance into temperature
+
+        Args:
+            resistance (_type_): _description_
+            mtr_coef (list, optional): _description_. Defaults to [0,0,0].
+
+        Returns:
+            _type_: _description_
+        """
+        if resistance <= 0:
+            shhh = 0
+        else:
+            shhh = (
+                1.0
+                / (
+                    float(mtr_coef[0])
+                    + (float(mtr_coef[1]) * np.log10(resistance))
+                    + (float(mtr_coef[2]) * np.log10(resistance) ** 3)
+                )
+                - 273.15
+            )
+
+        return shhh
+
+    def dic2df(self, data_dic):
+        """_summary_
+
+        Args:
+            data_dic (_type_): MTR data dictionary
+        """
+
+        ### create time and data streams
+        count = 0
+        time = {}
+        temp = {}
+        deltat = 0  # 10 min usually
+        for sam_num in data_dic:
+            time[count] = data_dic[sam_num]["time"]
+            try:
+                deltat = (
+                    data_dic[count + 1]["time"] - data_dic[count]["time"]
+                )  # in seconds
+                deltat = deltat / 120  # number of samples per record and convert
+            except:
+                datetime.timedelta(0)
+            # loop through dictionary rows
+            for i_row in range(0, 10, 1):  # loop through rows
+                for i_col in range(0, 12, 1):
+                    temp[count] = data_dic[sam_num]["resistance_" + str(i_row)][i_col]
+                    count += 1
+                    time[count] = time[count - 1] + deltat
+
+        df = pd.DataFrame(temp.values(),list(time.values())[:-1]) #last time stamp is an error in routine
+        df.index.rename('date_time',inplace=True)
+        df.rename(columns = {0:'temperature'}, inplace = True)
+
+        return df
--- src/EcoFOCIpy/io/rcm_parser.py
+++ src/EcoFOCIpy/io/rcm_parser.py
@@ -7,9 +7,9 @@ These include:
 
 """
 import pandas as pd
+import numpy as np
 
-
-class rcm(object):
+class rcm_excel(object):
     r""" Anderaa instruments (RCM 4, 7, 9, 11's
     EcoFOCI QC procedure developed by Dave P. and done within excel spreadsheet
 
@@ -32,6 +32,109 @@ class rcm(object):
             rawdata_df = rawdata_df.set_index(pd.DatetimeIndex(rawdata_df['date_time'])).drop(['date_time',"date/time"],axis=1)        
 
         return rawdata_df
+
+class rcm(object):
+    r"""
+    Evolution of the old RCM 7/9/11 Procedure.  Output *.dsu file to *.dat (tsv/csv) and process without excel worksheets.
+
+    Some notes below to convert dsu recorded counts to engr units.
+    RCM Oxygen 0.4883*counts = concentration (with internally set salinity.  FOCI standard is 0PSU).  D.Pashinski also had an equation to calculate percent saturation, but this needs to be validated? Ultimately, recalculate after salinity correction is applied.
+
+    Pressure, Turb, Cond/Sal, Temp have a transfer equation and coefs that need to be applied and these are unique to each unit (oxy are cal'd on instrument ad output a scaled value that is rcm independent, U/V comps are cal'd to the board and also output a alue that is already scaled)
+    """
+
+    def parse(self, filename=None, number_of_channels=8, datetime_index=True):
+        r"""
+        Basic Method to open and read converted dsu files 
+
+        returns pandas dataframe
+        """
+        assert filename != None , 'Must provide a datafile'
+
+        if number_of_channels == 8:
+            headernames=['sample','date','time','ident','speed_engr','dir_engr','temp_engr','cond_engr','press_engr','chan7','chan8']
+        elif number_of_channels == 7:
+            headernames=['sample','date','time','ident','speed_engr','dir_engr','temp_engr','cond_engr','press_engr','chan7']
+        else:
+            headernames=['sample','date','time','ident','speed_engr','dir_engr','temp_engr','cond_engr','press_engr']
+
+        rawdata_df = pd.read_csv(filename, names=headernames, delimiter="\s+",)
+        rawdata_df["date_time"] = pd.to_datetime(
+            rawdata_df['date'] + " " + rawdata_df['time'], format="%m/%d/%Y %H:%M:%S"
+        )
+        rawdata_df['temp_engr'] = pd.to_numeric(rawdata_df['temp_engr'],errors='coerce')
+
+        if datetime_index:
+            rawdata_df = rawdata_df.set_index(pd.DatetimeIndex(rawdata_df['date_time'])).drop(['date_time','date','time'],axis=1)
+
+        self.rawdata_df = rawdata_df
+
+        return (self.rawdata_df)
+
+    def engr2sci_curr(self):
+        """
+        Convert speed/dir
+
+        """
+        self.rawdata_df['current_speed'] = self.rawdata_df['speed_engr']*0.2933
+        self.rawdata_df['current_direction_uncorrected'] = self.rawdata_df['dir_engr']*0.3515
+        self.rawdata_df['u_curr_comp'] = self.rawdata_df['current_speed']*np.sin(np.deg2rad(self.rawdata_df['current_direction_uncorrected']))
+        self.rawdata_df['v_curr_comp'] = self.rawdata_df['current_speed']*np.cos(np.deg2rad(self.rawdata_df['current_direction_uncorrected']))
+
+    def engr2sci_oxy(self,channel='chan8'):
+        """
+        Convert speed/dir
+
+        """
+        if channel=='chan8':
+            self.rawdata_df['oxy_conc'] = self.rawdata_df['chan8']*0.4883
+        elif channel=='chan7':
+            self.rawdata_df['oxy_conc'] = self.rawdata_df['chan7']*0.4883
+
+    def engr2sci_temp(self,coefA=0,coefB=0,coefC=0,coefD=0):
+        """
+        Convert speed/dir
+
+        """
+        self.rawdata_df['temperature'] = coefA + coefB*self.rawdata_df['temp_engr'] + \
+            + coefC*(self.rawdata_df['temp_engr']**2) + coefD*(self.rawdata_df['temp_engr']**3)
+
+    def engr2sci_pres(self,coefA=0,coefB=0,coefC=0,equationType='low'):
+
+        if equationType=='low':
+           self.rawdata_df['pressure'] =(coefA+coefB*self.rawdata_df['press_engr']+coefC*(self.rawdata_df['temp_engr']**2))/10-10
+
+    def mag_dec_corr(self,lat,lonW,dep_date,apply_correction=True):
+        """Calculate mag declinatin correction based on lat, lon (+ West) and date.
+
+        Uses WMM_2020.COF - this model file updates every 5 years for current data.  Be sure to update this in future
+
+        Args:
+            lat (float): [description]
+            lonW (float): [description]
+            dep_date (datetime): [description]
+            apply_correction (boolean): correct the u,v for mag dec.  False just reports back the correction angle.
+
+        Returns:
+            float: [description]
+        """
+
+        import ecofocipy.math.geomag.geomag.geomag as geomag
+        import ecofocipy.math.geotools as geotools
+
+        t = geomag.GeoMag()
+        dec = t.GeoMag(lat,-1 * lonW,time=dep_date).dec
+
+        (u,v) = geotools.rotate_coord(self.rawdata_df['u_curr_comp'],self.rawdata_df['v_curr_comp'],dec)
+
+        self.rawdata_df['u_curr_comp'] = u
+        self.rawdata_df['v_curr_comp'] = v
+
+        return (self.rawdata_df,dec)
+
+    def get_data(self):
+
+        return (self.rawdata_df)
 
 class rcm_sg(object):    
     r""" Anderaa instruments (RCM Seaguards
