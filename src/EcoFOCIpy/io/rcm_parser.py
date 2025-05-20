9e05be0d724df9f9a2a145d1818e6b7762f240cc
shaunwbell <shaun.bell@noaa.gov>
2025-05-20 08:10:30 -0700

    Update rcm_parser.py (#380)
    deepwater addition

--- src/EcoFOCIpy/io/rcm_parser.py
+++ src/EcoFOCIpy/io/rcm_parser.py
@@ -1,4 +1,5 @@
-"""Contains a collection of MTR equipment parsing.
+"""
+Contains a collection of RCM equipment parsing.
 
 These include:
 
@@ -6,12 +7,13 @@ These include:
 * Version 5 (MTRduino) [x]
 
 """
+
 import numpy as np
 import pandas as pd
 
 
 class rcm_excel(object):
-    r""" Anderaa instruments (RCM 4, 7, 9, 11's
+    r"""Anderaa instruments (RCM 4, 7, 9, 11's
     EcoFOCI QC procedure developed by Dave P. and done within excel spreadsheet
 
     Dave P. usually performed the engr conversions and trimmed the data via excel
@@ -26,13 +28,18 @@ class rcm_excel(object):
         Basic Method to open and read rcm excel files
         """
 
-        rawdata_df = pd.read_excel(filename, skiprows=4, parse_dates=["date/time"], index_col="date/time")
+        rawdata_df = pd.read_excel(
+            filename, skiprows=4, parse_dates=["date/time"], index_col="date/time"
+        )
         rawdata_df.rename_axis("date_time", inplace=True)
 
         if datetime_index:
-            rawdata_df = rawdata_df.set_index(pd.DatetimeIndex(rawdata_df['date_time'])).drop(['date_time',"date/time"],axis=1)        
+            rawdata_df = rawdata_df.set_index(
+                pd.DatetimeIndex(rawdata_df["date_time"])
+            ).drop(["date_time", "date/time"], axis=1)
 
         return rawdata_df
+
 
 class rcm(object):
     r"""
@@ -44,95 +51,154 @@ class rcm(object):
     Pressure, Turb, Cond/Sal, Temp have a transfer equation and coefs that need to be applied and these are unique to each unit (oxy are cal'd on instrument ad output a scaled value that is rcm independent, U/V comps are cal'd to the board and also output a alue that is already scaled)
     """
 
-    def parse(self, filename=None, number_of_channels=8, time_format=0, datetime_index=True):
+    def parse(
+        self, filename=None, number_of_channels=8, time_format=0, datetime_index=True
+    ):
         r"""
-        Basic Method to open and read converted dsu files 
+        Basic Method to open and read converted dsu files
 
         time_format: 0 - %m/%d/%Y %H:%M:%S
         time_format: 1 - %d.%m.%Y %H:%M:%S
 
         returns pandas dataframe
         """
-        assert filename != None , 'Must provide a datafile'
-        assert time_format <=1 , 'time_format must be 0,1'
+        assert filename != None, "Must provide a datafile"
+        assert time_format <= 1, "time_format must be 0,1"
         if number_of_channels == 8:
-            headernames=['sample','date','time','ident','speed_engr','dir_engr','temp_engr','cond_engr','press_engr','chan7','chan8']
+            headernames = [
+                "sample",
+                "date",
+                "time",
+                "ident",
+                "speed_engr",
+                "dir_engr",
+                "temp_engr",
+                "cond_engr",
+                "press_engr",
+                "chan7",
+                "chan8",
+            ]
         elif number_of_channels == 7:
-            headernames=['sample','date','time','ident','speed_engr','dir_engr','temp_engr','cond_engr','press_engr','chan7']
+            headernames = [
+                "sample",
+                "date",
+                "time",
+                "ident",
+                "speed_engr",
+                "dir_engr",
+                "temp_engr",
+                "cond_engr",
+                "press_engr",
+                "chan7",
+            ]
         else:
-            headernames=['sample','date','time','ident','speed_engr','dir_engr','temp_engr','cond_engr','press_engr']
+            headernames = [
+                "sample",
+                "date",
+                "time",
+                "ident",
+                "speed_engr",
+                "dir_engr",
+                "temp_engr",
+                "cond_engr",
+                "press_engr",
+            ]
 
-        rawdata_df = pd.read_csv(filename, names=headernames, delimiter=r'\s+',)
+        rawdata_df = pd.read_csv(
+            filename,
+            names=headernames,
+            delimiter=r"\s+",
+        )
         if time_format == 0:
             rawdata_df["date_time"] = pd.to_datetime(
-                rawdata_df['date'] + " " + rawdata_df['time'], format="%m/%d/%Y %H:%M:%S"
+                rawdata_df["date"] + " " + rawdata_df["time"],
+                format="%m/%d/%Y %H:%M:%S",
             )
         elif time_format == 1:
             rawdata_df["date_time"] = pd.to_datetime(
-                rawdata_df['date'] + " " + rawdata_df['time'], format="%d.%m.%Y %H:%M:%S"
+                rawdata_df["date"] + " " + rawdata_df["time"],
+                format="%d.%m.%Y %H:%M:%S",
             )
         else:
             pass
-        rawdata_df['temp_engr'] = pd.to_numeric(rawdata_df['temp_engr'],errors='coerce')
+        rawdata_df["temp_engr"] = pd.to_numeric(
+            rawdata_df["temp_engr"], errors="coerce"
+        )
 
         if datetime_index:
-            rawdata_df = rawdata_df.set_index(pd.DatetimeIndex(rawdata_df['date_time'])).drop(['date_time','date','time'],axis=1)
+            rawdata_df = rawdata_df.set_index(
+                pd.DatetimeIndex(rawdata_df["date_time"])
+            ).drop(["date_time", "date", "time"], axis=1)
 
         self.rawdata_df = rawdata_df
 
-        return (self.rawdata_df)
+        return self.rawdata_df
 
     def drop_headerrows(self, ident=None):
         """
         keep the row that identifies the data, dropping all other rows
         """
-        self.rawdata_df = self.rawdata_df[self.rawdata_df['ident'] == ident] #<-- there is a value that represents data and one that represents headers, drop all the headers
+        self.rawdata_df = self.rawdata_df[
+            self.rawdata_df["ident"] == ident
+        ]  # <-- there is a value that represents data and one that represents headers, drop all the headers
 
     def engr2sci_curr(self):
         """
         Convert speed/dir
 
         """
-        self.rawdata_df['current_speed'] = self.rawdata_df['speed_engr']*0.2933
-        self.rawdata_df['current_direction_uncorrected'] = self.rawdata_df['dir_engr']*0.3515
-        self.rawdata_df['u_curr_comp'] = self.rawdata_df['current_speed']*np.sin(np.deg2rad(self.rawdata_df['current_direction_uncorrected']))
-        self.rawdata_df['v_curr_comp'] = self.rawdata_df['current_speed']*np.cos(np.deg2rad(self.rawdata_df['current_direction_uncorrected']))
+        self.rawdata_df["current_speed"] = self.rawdata_df["speed_engr"] * 0.2933
+        self.rawdata_df["current_direction_uncorrected"] = (
+            self.rawdata_df["dir_engr"] * 0.3515
+        )
+        self.rawdata_df["u_curr_comp"] = self.rawdata_df["current_speed"] * np.sin(
+            np.deg2rad(self.rawdata_df["current_direction_uncorrected"])
+        )
+        self.rawdata_df["v_curr_comp"] = self.rawdata_df["current_speed"] * np.cos(
+            np.deg2rad(self.rawdata_df["current_direction_uncorrected"])
+        )
 
-    def engr2sci_oxy(self,channel='chan8'):
+    def engr2sci_oxy(self, channel="chan8"):
         """
         Convert oxygen
 
         """
-        if channel=='chan8':
-            self.rawdata_df['oxy_conc'] = self.rawdata_df['chan8']*0.4883
-            self.rawdata_df['oxy_percentsat'] = self.rawdata_df['chan8']*0.1465
-        elif channel=='chan7':
-            self.rawdata_df['oxy_conc'] = self.rawdata_df['chan7']*0.4883
-            self.rawdata_df['oxy_percentsat'] = self.rawdata_df['chan7']*0.1465
+        if channel == "chan8":
+            self.rawdata_df["oxy_conc"] = self.rawdata_df["chan8"] * 0.4883
+            self.rawdata_df["oxy_percentsat"] = self.rawdata_df["chan8"] * 0.1465
+        elif channel == "chan7":
+            self.rawdata_df["oxy_conc"] = self.rawdata_df["chan7"] * 0.4883
+            self.rawdata_df["oxy_percentsat"] = self.rawdata_df["chan7"] * 0.1465
 
-    def engr2sci_temp(self,coefA=0,coefB=0,coefC=0,coefD=0):
+    def engr2sci_temp(self, coefA=0, coefB=0, coefC=0, coefD=0):
         """
         Convert temperature
 
         """
-        self.rawdata_df['temperature'] = coefA + coefB*self.rawdata_df['temp_engr'] + \
-            + coefC*(self.rawdata_df['temp_engr']**2) + coefD*(self.rawdata_df['temp_engr']**3)
+        self.rawdata_df["temperature"] = (
+            coefA
+            + coefB * self.rawdata_df["temp_engr"]
+            + +coefC * (self.rawdata_df["temp_engr"] ** 2)
+            + coefD * (self.rawdata_df["temp_engr"] ** 3)
+        )
 
-    def engr2sci_pres(self,coefA=0,coefB=0,coefC=0,equationType='low',units='kPa'):
+    def engr2sci_pres(self, coefA=0, coefB=0, coefC=0, equationType="low", units="kPa"):
         """
         Convert pressure
 
         """
-        if equationType=='low':
-           self.rawdata_df['pressure'] =(coefA+coefB*self.rawdata_df['press_engr']+coefC*(self.rawdata_df['press_engr']**2))
-           if units=='MPa':
-               self.rawdata_df['pressure'] =self.rawdata_df['pressure']*100-10
-           elif units=='kPa':
-               self.rawdata_df['pressure'] =self.rawdata_df['pressure']/10-10
+        if equationType == "low":
+            self.rawdata_df["pressure"] = (
+                coefA
+                + coefB * self.rawdata_df["press_engr"]
+                + coefC * (self.rawdata_df["press_engr"] ** 2)
+            )
+            if units == "MPa":
+                self.rawdata_df["pressure"] = self.rawdata_df["pressure"] * 100 - 10
+            elif units == "kPa":
+                self.rawdata_df["pressure"] = self.rawdata_df["pressure"] / 10 - 10
 
-            
-
-    def mag_dec_corr(self,lat,lonW,dep_date,apply_correction=True):
+    def mag_dec_corr(self, lat, lonW, dep_date, apply_correction=True):
         """Calculate mag declinatin correction based on lat, lon (+ West) and date.
 
         Uses WMM_2020.COF - this model file updates every 5 years for current data.  Be sure to update this in future
@@ -151,28 +217,32 @@ class rcm(object):
         import EcoFOCIpy.math.geotools as geotools
 
         t = geomag.GeoMag()
-        dec = t.GeoMag(lat,-1 * lonW,time=dep_date).dec
+        dec = t.GeoMag(lat, -1 * lonW, time=dep_date).dec
 
-        (u,v) = geotools.rotate_coord(self.rawdata_df['u_curr_comp'],self.rawdata_df['v_curr_comp'],dec)
+        (u, v) = geotools.rotate_coord(
+            self.rawdata_df["u_curr_comp"], self.rawdata_df["v_curr_comp"], dec
+        )
 
-        self.rawdata_df['u_curr_comp'] = u
-        self.rawdata_df['v_curr_comp'] = v
+        self.rawdata_df["u_curr_comp"] = u
+        self.rawdata_df["v_curr_comp"] = v
 
-        return (self.rawdata_df,dec)
+        return (self.rawdata_df, dec)
 
     def get_data(self):
 
-        return (self.rawdata_df)
+        return self.rawdata_df
 
-class rcm_sg(object):    
-    r""" Anderaa instruments (RCM Seaguards
-    These datastreams are no longer binary off the instrument and can be qc'd by the aandera software,
-    This means that a preliminary qc'd product is archived in netcdf via output files.
 
-    Due to the Seaguard being a platform, multiple datafiles are possible, usually including oxygen but not always
+class rcm_sg(object):
+    r"""Anderaa instruments (RCM Seaguards
+    These datastreams are no longer binary off the instrument and can be qc'd by the aandera
+    software This means that a preliminary qc'd product is archived in netcdf via output files.
+
+    Due to the Seaguard being a platform, multiple datafiles are possible, usually including
+    oxygen but not always
 
     TODO: This should be a replacement of the original rcm mooring analyis software"""
-    
+
     @staticmethod
     def parse_excel(filename=None, datetime_index=True):
         r"""
@@ -181,15 +251,23 @@ class rcm_sg(object):
 
         rawdata_df = pd.read_csv(filename, header=1, delimiter="\t")
         try:
-            rawdata_df["date_time"] = pd.to_datetime(rawdata_df["Time tag (Gmt)"], format="%d.%m.%y %H:%M:%S")
-        except:
-            rawdata_df["date_time"] = pd.to_datetime(rawdata_df["Record Time"], format="%m/%d/%y %H:%M:%S")
+            rawdata_df["date_time"] = pd.to_datetime(
+                rawdata_df["Time tag (Gmt)"], format="%d.%m.%y %H:%M:%S"
+            )
+        except KeyError:
+            rawdata_df["date_time"] = pd.to_datetime(
+                rawdata_df["Record Time"], format="%m/%d/%y %H:%M:%S"
+            )
 
         if datetime_index:
             try:
-                rawdata_df = rawdata_df.set_index(pd.DatetimeIndex(rawdata_df['date_time'])).drop(['date_time','Time tag (Gmt)'],axis=1)        
-            except:
-                rawdata_df = rawdata_df.set_index(pd.DatetimeIndex(rawdata_df['date_time'])).drop(['date_time','Record Time'],axis=1)        
+                rawdata_df = rawdata_df.set_index(
+                    pd.DatetimeIndex(rawdata_df["date_time"])
+                ).drop(["date_time", "Time tag (Gmt)"], axis=1)
+            except KeyError:
+                rawdata_df = rawdata_df.set_index(
+                    pd.DatetimeIndex(rawdata_df["date_time"])
+                ).drop(["date_time", "Record Time"], axis=1)
 
         return rawdata_df
 
@@ -199,7 +277,7 @@ class rcm_sg(object):
         Basic Method to open and read rcm text files
         """
 
-        assert filename != None , 'Must provide a datafile'
+        assert filename != None, "Must provide a datafile"
 
         header = []
 
@@ -207,25 +285,32 @@ class rcm_sg(object):
             for k, line in enumerate(fobj.readlines()):
                 header = header + [line]
                 if "Record" in line:
-                    headercount=k
+                    headercount = k
                     break
 
-
-        rawdata_df = pd.read_csv(filename, 
-                        delimiter="\t", 
-                        parse_dates=True, 
-                        skiprows=headercount)
-        rawdata_df["date_time"] = pd.to_datetime(rawdata_df["Time tag (Gmt)"], format="%d.%m.%Y %H:%M:%S")
+        rawdata_df = pd.read_csv(
+            filename, delimiter="\t", parse_dates=True, skiprows=headercount
+        )
+        try:
+            rawdata_df["date_time"] = pd.to_datetime(
+                rawdata_df["Time tag (Gmt)"], format="%d.%m.%Y %H:%M:%S"
+            )
+        except KeyError:
+            rawdata_df["date_time"] = pd.to_datetime(
+                rawdata_df["Record Time"], format="%m/%d/%y %H:%M:%S"
+            )
 
         if datetime_index:
-            rawdata_df = rawdata_df.set_index(pd.DatetimeIndex(rawdata_df['date_time'])).drop(['date_time',"Time tag (Gmt)"],axis=1)        
+            rawdata_df = rawdata_df.set_index(
+                pd.DatetimeIndex(rawdata_df["date_time"])
+            ).drop(["date_time", "Time tag (Gmt)"], axis=1)
 
-        return (rawdata_df,header)
+        return (rawdata_df, header)
 
-    def load(self,data):
+    def load(self, data):
         self.rawdata_df = data
 
-    def mag_dec_corr(self,lat,lonW,dep_date,apply_correction=True):
+    def mag_dec_corr(self, lat, lonW, dep_date, apply_correction=True):
         """Calculate mag declinatin correction based on lat, lon (+ West) and date.
 
         Uses WMM_2020.COF - this model file updates every 5 years for current data.  Be sure to update this in future
@@ -244,11 +329,13 @@ class rcm_sg(object):
         import EcoFOCIpy.math.geotools as geotools
 
         t = geomag.GeoMag()
-        dec = t.GeoMag(lat,-1 * lonW,time=dep_date).dec
+        dec = t.GeoMag(lat, -1 * lonW, time=dep_date).dec
 
-        (u,v) = geotools.rotate_coord(self.rawdata_df['u_curr_comp'],self.rawdata_df['v_curr_comp'],dec)
+        (u, v) = geotools.rotate_coord(
+            self.rawdata_df["u_curr_comp"], self.rawdata_df["v_curr_comp"], dec
+        )
 
-        self.rawdata_df['u_curr_comp'] = u
-        self.rawdata_df['v_curr_comp'] = v
+        self.rawdata_df["u_curr_comp"] = u
+        self.rawdata_df["v_curr_comp"] = v
 
-        return (self.rawdata_df,dec)
+        return (self.rawdata_df, dec)
